bde8c643dcdbff0136572386a0cdb0aeabb43319
Gabriel Diaz <gdiaz1410@gmail.com>
2017-06-15 00:17:27 -0500

    update # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- app.js
+++ app.js
@@ -14,24 +14,39 @@ var score, roundScore, activePlayer;
 
 score = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // random number
     var dice = Math.floor(Math.random() * 6) + 1;
     // display result
-    document.querySelector('.dice').style.display = 'block';
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
     // update round score if rolled number was not 1
+    if (dice !== 1) {
+      // add score
+      roundScore += dice;
+      // roundScore = roundScore + dice; same
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      // next player
+      // **** if active is 0 then swith to 1, else go to 0
+      activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+      roundScore = 0;
+
+      document.getElementById('current-0').textContent = '0';
+      document.getElementById('current-1').textContent = '0';
+    }
 });
 
 
@@ -42,6 +57,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-
-// .
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
